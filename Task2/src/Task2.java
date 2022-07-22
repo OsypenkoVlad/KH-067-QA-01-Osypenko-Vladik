aed1345f555e076cc64d5c6c7d0dd92b635dd2f1
Osypenkovlad <osypenkovlad95@gmail.com>
2022-07-22 12:29:40 +0300

    modified:   Task2/src/Task2.java

--- Task2/src/Task2.java
+++ Task2/src/Task2.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
-public class Task2 { public static void main(String[]args) {
-    Scanner scanner = new Scanner(System.in);
-    String s1, s2;
-    System.out.println("What is your name?");
-    s1 = scanner.nextLine();
-    System.out.println("Where are you live?");
-    s2 = scanner.nextLine();
-    System.out.println("Congratulations!");
-    System.out.println(("Your name is:") + s1);
-    System.out.println(("You live in:") + s2);
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String variable1, variable2;
+        System.out.println("What is your name?");
+        variable1 = scanner.nextLine();
+        System.out.println("Where are you live?");
+        variable2 = scanner.nextLine();
+        System.out.println("Congratulations!");
+        System.out.println(("Your name is:") + variable1);
+        System.out.println(("You live in:") + variable2);
 
-}
+    }
 }
